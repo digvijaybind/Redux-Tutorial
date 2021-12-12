c160c99f972e24ebf343503d2974d0fb7ac01fe2
Digvijay <binddigvijay123@gmail.com>
2021-12-12 17:49:52 +0530

    this is inital commit

---
+++ src/User.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const User=() =>{
+
+return(
+
+  <div>
+
+
+  <h1>this is digvijay</h1>
+  </div>
+
+
+)
+
+}
+
+export default User;
---
+++ src/components/Home.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function Home(){
+
+
+    return (
+
+
+
+        <div>
+
+            <h1> Home components</h1>
+
+            <div className="cart-wrapper">
+
+                <div className="img-wrapper item">
+
+                 
+
+                    
+                </div>
+
+                <div className="text-wrapper item">
+                   <span>
+                       I-phone
+                   </span>
+    <span>
+
+        Price:$1000.0
+    </span>
+
+                    </div>
+                    <div className="btn-wrapper item">
+                    
+                    <button>  Add to cart</button>
+                     
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Home;
+
